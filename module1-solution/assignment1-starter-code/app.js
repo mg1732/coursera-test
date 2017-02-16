a6dad304cfec3af6e9cc117e899eeec36523b84e
mg <you@example.com>
2017-02-16 10:17:17 +0200

    module1-solution upload

---
+++ module1-solution/assignment1-starter-code/app.js
@@ -0,0 +1,26 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+function LunchCheckController ($scope,
+                       $filter,
+                       $injector) {
+  $scope.dishes = "";
+  $scope.calcMessage = "abc";
+
+  $scope.calcDishes = function () {
+    var dishesCount = $scope.dishes.split(',').filter(Boolean).length;
+    if (dishesCount == 0) {
+      $scope.calcMessage = $scope.dishes;
+    } else if (dishesCount <= 3) {
+      $scope.calcMessage = "Enjoy!";
+    } else {
+      $scope.calcMessage = "Too much!";
+    }
+  };
+
+
+}
+})();
